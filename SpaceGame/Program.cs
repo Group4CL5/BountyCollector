be42cea400c4ddc00abd376655f14b1e0a2ce80e
Francisco Santillana <francisco.santillana@outlook.com>
2020-02-08 19:42:25 -0500

    Background Music
    I added the code for the background music. Check it out!!

--- SpaceGame/Program.cs
+++ SpaceGame/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SpaceGameClassLibrary;
+using System.Media; //Francisco: This allows the background music
 
 namespace SpaceGame
 {
@@ -11,6 +12,11 @@ namespace SpaceGame
     {
         static void Main()
         {
+            //Francisco: Below is the code that allows the beat "BountyHunter.wav" to be played in background.
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "BountyCollector.wav";
+            player.Play();
+            //Francisco: The code ends here for "BountyHunter.wav" background music.
             try
             {
                 GameManager gameManager = new GameManager();
