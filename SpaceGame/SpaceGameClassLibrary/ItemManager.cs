999fbb84a368102851658d96bf918a55a3e8f438
dlharris1029 <60747576+dlharris1029@users.noreply.github.com>
2020-02-10 16:15:59 -0500

    Shop Update

---
+++ SpaceGame/SpaceGameClassLibrary/ItemManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGameClassLibrary
+{
+    class ItemManager
+    {
+        private List<Items> items = new List<Items>();
+
+        public ItemManager()
+        {
+            Items item1 = new Items();
+            item1.Cost = 0;
+            item1.Name = "test1";
+            items.Add(item1);
+
+            Items item2 = new Items();
+            item2.Cost = 10;
+            item2.Name = "test2";
+            items.Add(item2);
+
+            Items item3 = new Items();
+            item3.Cost = 20;
+            item3.Name = "test3";
+            items.Add(item3);
+        }
+
+        public List<Items> ReturnItems() {
+            return items;
+        }
+    }
+}
