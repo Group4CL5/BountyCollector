277301397484be9ccda683783ee393ff6042dfc8
dlharris1029 <60747576+dlharris1029@users.noreply.github.com>
2020-02-12 11:41:53 -0500

    Test

--- SpaceGame/SpaceGameClassLibrary/Shop.cs
+++ SpaceGame/SpaceGameClassLibrary/Shop.cs
@@ -8,18 +8,42 @@ namespace SpaceGameClassLibrary
 {
     class Shop
     {
+<<<<<<< HEAD
+        ItemManager itemManager;
+
+        public Shop()
+        {
+            itemManager = new ItemManager();
+        }
+        public string ShowShop(Player player)
+=======
        
-        public static string ShowShop(CoinManager coinManager)
+        public static string ShowShop(Player player)
+>>>>>>> 8a7f6fc7b493ba7be3a7c7be43d136efb5ceb808
         {
             string outputText = "";
-            ItemManager itemManager = new ItemManager();
-
+            
             foreach(Items item in itemManager.ReturnItems())
             {
                 outputText += $"Ship {item.Name} costs {item.Cost}\n";
             }
 
             return outputText;
+        }
+
+        public bool BuyItem(int ItemNum, Player player)
+        {
+
+
+            if (player.Coins >= itemManager.ReturnItem(ItemNum).Cost)
+            {
+                player.Item++;
+                return true;
+
+            }
+            else
+                return false;
+
         }
 
 
